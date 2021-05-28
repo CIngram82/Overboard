811505e8bdf527e31f54b6d673c7dead8f942a1c
Robert <csecoura@gmail.com>
2021-05-27 20:56:07 -0400

    Dragging Inventory
    DragHandler allows UI object to be moved.
    DropHandler receives Draggable objects.

---
+++ Assets/Scripts/DragHandler.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public static GameObject Item;
+    Vector3 startPos;
+    Transform startParent;
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Item = gameObject;
+        startPos = transform.position;
+        startParent = transform.parent;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Item = null;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+        if (transform.parent == startParent)
+        {
+            transform.position = startPos;
+        }
+    }
+}
+
+namespace UnityEngine.EventSystems
+{
+    public interface IHasChanged : IEventSystemHandler
+    {
+        void HasChanged();
+    }
+}
+
+
+
+
+
---
+++ Assets/Scripts/DropHandler.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropHandler : MonoBehaviour, IDropHandler
+{
+    public GameObject Item
+    {
+        get
+        {
+            if (transform.childCount > 0)
+            {
+                return transform.GetChild(0).gameObject;
+            }
+            return null;
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (!Item)
+        {
+            DragHandler.Item.transform.SetParent(transform);
+            ExecuteEvents.ExecuteHierarchy<IHasChanged>(gameObject, null, (x, y) => x.HasChanged());
+        }
+    }
+}
+
+
+
+
+
