5b557c5a0cef31ee21101192787154668f7f25c2
Destiney <50345556+SycoStorm@users.noreply.github.com>
2021-05-28 21:22:27 -0400

    Fixed mouseY, code cleanup
    Took out code that was not needed, Fixed the camera not being able to look up.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,7 +9,6 @@ public class CameraController : MonoBehaviour
     float mouseY;
     bool hideMouse;
     [SerializeField] Transform Player;
-    [SerializeField] Transform forward;
 
     private void Start()
     {
@@ -25,33 +24,9 @@ public class CameraController : MonoBehaviour
     void CameraControls()
     {
         mouseX += Input.GetAxis("Mouse X") * rotSpeed;
-        
-        // What is the mouse Y doing? In game you cant look up/down
-
         mouseY -= Input.GetAxis("Mouse Y") * rotSpeed;
-        mouseY = Mathf.Clamp(mouseY, -35, -60);
-        // inspect the Forward object and play the game. 
-        // It flickers the x rotation from -35 to -60 over and over. 
-        forward.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        gameObject.transform.rotation = Quaternion.Euler(Mathf.Clamp(mouseY * rotSpeed, -30f, 40f), mouseX, 0);
         Player.rotation = Quaternion.Euler(0, mouseX, 0);
-        
-        /*
-        if(Input.GetMouseButton(1) && hideMouse)
-        {
-            Cursor.visible = true;
-            Cursor.lockState = CursorLockMode.None;
-            hideMouse = false;
-        }
-        else if(Input.GetMouseButton(1) && !hideMouse)
-        {
-            Cursor.visible = false;
-            Cursor.lockState = CursorLockMode.Locked;
-            hideMouse = true;
-        }
-        */
-        
-        //This can be made into a one if statment 
-        // Also use the ButtonDown so it does not flicker if the mouse is held down. 
         
         if (Input.GetMouseButtonDown(1))
         {
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,26 +4,14 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-    public LayerMask groundLayer;
-
-    [SerializeField] float jumpForce;
     [SerializeField] float speed;
-    // Why have the 2 below as public they are not used in any other script.
-    public float moveHorizontal;
-    public float moveVertical;
-
-    // What are the below 3 used for?
-    // If not needed please remove them. 
-    Rigidbody rig;
-    float yMovement;
-    bool canJump;
+    float moveHorizontal;
+    float moveVertical;
 
     [SerializeField] Animator camAnim;
 
     void Start()
     {
-        rig = GetComponent<Rigidbody>();
-        canJump = true;
         speed = 5;
     }
 
@@ -34,18 +22,6 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical) * speed * Time.deltaTime;
         transform.Translate(movement, Space.Self);
-        /*
-        if (moveHorizontal != 0 || moveVertical != 0)
-        {
-            camAnim.SetBool("isWalking", true);
-        }
-        else
-        {
-            camAnim.SetBool("isWalking", false);
-        }
-        */
-        // This can be cleaned up into 1 line of code.
-
         camAnim.SetBool("isWalking", moveHorizontal != 0 || moveVertical != 0);
     }
 }
