bc3444f01afec19bb74a29815b0174ee9e472811
Destiney <50345556+SycoStorm@users.noreply.github.com>
2021-06-04 22:27:23 -0400

    Fixed the lockbox to be more specific
    The lock box  only checked for a specific amount of collectables instead of specific ones, this had the potential to cause false positives so I fixed it. The clue panel got deleted from scene so I remade it AND made it a prefab so it can be used in other scenes to show the clues for the player.

--- Assets/Project/Runtime/Scripts/StateRoomScripts/GrabObject.cs
+++ Assets/Project/Runtime/Scripts/StateRoomScripts/GrabObject.cs
@@ -11,9 +11,9 @@ public class GrabObject : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Confined; // this needs to be changed on the camera script when I update that branch and taken out of this one, but the cursor leaving the screen was causing a lot of issues with double monitor.
         RaycastHit rayHit;
-        if (Physics.Raycast(transform.position, transform.forward, out rayHit, 50, collectables) && Input.GetMouseButtonDown(0))
+        if (Physics.Raycast(transform.position, transform.forward, out rayHit, 20f, collectables) && Input.GetMouseButtonDown(0))
         {
-            collectablesList.Add(rayHit.transform.gameObject);
+            collectablesList.Add(rayHit.transform.gameObject);// this only takes the gameObject of the maguffin within the prefab not the entire prefab as that would require restructuring of the world item prefab to include the layer at base.
             rayHit.transform.gameObject.SetActive(false);
         }
        
--- Assets/Project/Runtime/Scripts/StateRoomScripts/LockBox.cs
+++ Assets/Project/Runtime/Scripts/StateRoomScripts/LockBox.cs
@@ -5,15 +5,34 @@ using UnityEngine;
 public class LockBox : MonoBehaviour
 {
     [SerializeField] GameObject key;
+    [SerializeField] List<GameObject> collectablesRequired = new List<GameObject>();
+   
 
     private void OnTriggerStay(Collider other)
     {
        
-        if (Input.GetMouseButton(0) && other.gameObject.CompareTag("Player") && GrabObject.collectablesList.Count >= 3)
+        if (Input.GetMouseButton(0) && other.gameObject.CompareTag("Player") && checkCollections() == true)
         {
             GrabObject.collectablesList.Clear(); // you probably wouldn't hold these items after use. 
             key.SetActive(true);
         }
+
+    }
+
+
+    bool checkCollections()
+    {
+        foreach (GameObject collectables in collectablesRequired)
+        {
+            if (!GrabObject.collectablesList.Contains(collectables))
+            {
+                Debug.Log( collectables.name + "This isnt in the collectionsList");
+                return false;
+            }
+
+        }
+        return true;
+
     }
 
 }
