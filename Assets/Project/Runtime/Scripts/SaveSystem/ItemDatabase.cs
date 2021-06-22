8b48995453dcbd8925c216b13cb85fa90a1f3a39
Robert <csecoura@gmail.com>
2021-06-21 22:19:28 -0400

    Updated Item DB
    Added prefab to item

--- Assets/Project/Runtime/Scripts/SaveSystem/ItemDatabase.cs
+++ Assets/Project/Runtime/Scripts/SaveSystem/ItemDatabase.cs
@@ -13,10 +13,12 @@ namespace Inventory.Database
     [System.Serializable]
     public class Item : InventoryItem
     {
+        [SerializeField] GameObject _prefab;
         [SerializeField] Image _image;
         [TextArea(5, 10)]
         [SerializeField] string _description;
 
+        public GameObject Prefab { get => _prefab; private set => _prefab = value; }
         public Image Image { get => _image; private set => _image = value; }
         public string Description { get => _description; private set => _description = value; }
 
