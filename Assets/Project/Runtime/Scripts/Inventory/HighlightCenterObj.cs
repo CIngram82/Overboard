961c038d99e67f2d842e61a00e0dc0246510d9e1
fellbeast9 <Wring8@gmail.com>
2021-07-03 13:26:02 -0400

    outline shader

---
+++ Assets/Project/Runtime/Scripts/Inventory/HighlightCenterObj.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightCenterObj : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
