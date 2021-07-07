1290187a4901f94ec34786d2d83e3f273b6485c2
christopher ingram <cingram82@gmail.com>
2021-07-07 10:22:12 -0400

    Open door on E or click working -- Ish
    TODO: test trigger volume to make it easer for the player

--- Assets/Project/Runtime/Scripts/LevelDesign/DoorTrigger.cs
+++ Assets/Project/Runtime/Scripts/LevelDesign/DoorTrigger.cs
@@ -1,24 +1,121 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DoorTrigger : MonoBehaviour
 {
     Animator anim;
+    private Camera cam;
+    private GameObject player;
+
+    public float reach = 1.5f; // How far can the player reach
+    private string animBoolName = "isOpen";
+    [SerializeField] private bool playerEntered;
+    [SerializeField] private bool showMessage;
+    private string message;
+    public TMP_Text messageGUI;
+
+    private int rayLayerMask;
 
     void Start()
     {
         anim = GetComponent<Animator>();
+        anim.enabled = false;
+        player = GameObject.FindGameObjectWithTag("Player");
+        cam = Camera.main;
+        Debug.Assert(cam != null, "Main camera not found");
+        // get the layer for the interact and invert the masking
+        LayerMask iRayLM = LayerMask.NameToLayer("Interact");
+        rayLayerMask = 1 << iRayLM.value;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        anim.SetBool("isOpen", true);
-        //AudioScript._instance.PlaySoundEffect("Door Creak");
+        if (other.gameObject == player)
+        {
+            playerEntered = true;
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        anim.SetBool("isOpen", false);
+        if (other.gameObject == player)
+        {
+            playerEntered = false;
+            showMessage = false;
+        }
+    }
+    void Update()
+    {
+        if (playerEntered)
+        {
+            Vector3 rayStart = cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+            RaycastHit hit;
+
+            if(Physics.Raycast(rayStart, cam.transform.forward, out hit, reach, rayLayerMask))
+            {
+                InteractableObject interactObj = isEqualToParent(hit.collider);
+                // is the player looking at this object or a child of it?
+
+                if(interactObj == null)
+                {
+                    // Not looking at us
+                    return;
+                }
+                else
+                {
+                    Debug.Log(interactObj.gameObject.name);
+                    showMessage = true;
+                    bool isOpen = anim.GetBool(animBoolName);
+                    if(Input.GetKeyUp(KeyCode.E) || Input.GetButtonDown("Fire1"))
+                    {
+                        anim.enabled = true;
+                        anim.SetBool(animBoolName, !isOpen);
+
+                    }
+
+                }
+
+            }
+            else
+            {
+                showMessage = false;
+            }
+        }
+    }
+
+    // look up the family tree of the object hit and see if one of them has the InteractableObject component
+    private InteractableObject isEqualToParent(Collider other)
+    {
+        InteractableObject interactObj = null;
+        try
+        {
+            int maxWalk = 5;
+            interactObj = other.GetComponent<InteractableObject>();
+
+            GameObject currentGO = other.gameObject;
+            for (int i = 0; i < maxWalk; i++)
+            {
+                // is the ray cast hitting the object with this script. 
+                if (currentGO.Equals(this.gameObject))
+                {
+                    interactObj = GetComponent<InteractableObject>();
+                    if (interactObj == null) interactObj = currentGO.GetComponentInParent<InteractableObject>();
+                    break;          //exit loop early.
+                }
+                //not equal to if reached this far in loop. move to parent if exists.
+                if (currentGO.transform.parent != null)     //is there a parent
+                {
+                    currentGO = currentGO.transform.parent.gameObject;
+                }
+            }
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+        return interactObj;
+
     }
 }
