cce005778bc7b5ee6f76c4c1326916873cefcbfe
fellbeast9 <Wring8@gmail.com>
2021-07-09 19:29:48 -0400

    Finished End Cutscene

--- Assets/tentacleAnimRandomizer.cs
+++ Assets/tentacleAnimRandomizer.cs
@@ -7,7 +7,7 @@ public class tentacleAnimRandomizer : StateMachineBehaviour
     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.SetInteger("AnimToPlay", Random.Range(1, 6));
+        animator.SetInteger("AnimToPlay", Random.Range(0, 3));
         animator.SetFloat("AnimWait", Random.Range(0.2f, 1));
     }
 
