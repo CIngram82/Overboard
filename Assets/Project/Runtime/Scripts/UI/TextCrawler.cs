7c91b8b7ca90e1e9a9be220642bd4955315dc46c
Robert <csecoura@gmail.com>
2021-07-10 18:29:06 -0400

    Credits scene update

--- Assets/Project/Runtime/Scripts/UI/TextCrawler.cs
+++ Assets/Project/Runtime/Scripts/UI/TextCrawler.cs
@@ -10,20 +10,24 @@ namespace Controllers.UI
         [SerializeField] TextMeshProUGUI text;
         [SerializeField] float scrollSpeed;
         [SerializeField] float transitionDelayTime;
-       
+
         WaitForSeconds transitionDelay;
         RectTransform textCrawler;
 
         IEnumerator TextCrawlerVert()
         {
             float scrollPos = 0.0f;
+            //float height = LayoutUtility.GetPreferredHeight(textCrawler);
             float height = textCrawler.sizeDelta.y;
             Vector3 startPos = textCrawler.position;
 
-            while (scrollPos < (height) + (Screen.height))
+
+            while (scrollPos < (height))
             {
+            Debug.LogWarning($"{scrollPos} < {height}");
+            Debug.LogWarning($"Position: {textCrawler.transform.position}");
                 scrollPos += scrollSpeed * Time.deltaTime;
-                textCrawler.position = new Vector3(startPos.x, scrollPos, startPos.z);
+                textCrawler.transform.position = new Vector3(startPos.x, scrollPos, startPos.z);
                 yield return null;
             }
             yield return transitionDelay;
@@ -33,13 +37,15 @@ namespace Controllers.UI
         void Awake()
         {
             textCrawler = GetComponent<RectTransform>();
-            //textCrawler.position = new Vector3(textCrawler.position.x, 0.0f, textCrawler.position.z);
+            textCrawler.position = new Vector3(textCrawler.position.x, 0.0f, textCrawler.position.z);
             transitionDelay = new WaitForSeconds(transitionDelayTime);
         }
 
         IEnumerator Start()
         {
-            yield return new WaitForSeconds(1.0f);
+            yield return new WaitForFixedUpdate();
+            LayoutRebuilder.ForceRebuildLayoutImmediate(textCrawler);
+
             StartCoroutine(TextCrawlerVert());
         }
     }
